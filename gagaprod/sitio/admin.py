e76e5872e07399f70dc55f28fd1092c13abd7541
Jango-7 <jhoanngo2@gmail.com>
2024-01-02 20:23:48 -0300

    update 3.1

--- gagaprod/sitio/admin.py
+++ gagaprod/sitio/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from sitio.models import *
 
 # Register your models here.
+admin.site.register(ProduccionesMod)
+admin.site.register(MultitracksMod)
+admin.site.register(ChartsMod)
